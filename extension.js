a0946ab3462186fa5bdf76e1f8e08ee72f53d71d
Ben Lavender <blavender@gmail.com>
2012-05-11 02:36:49 -0500

    initial commit

---
+++ extension.js
@@ -0,0 +1,15 @@
+(function() {
+  // keep header from chasing me down the page
+  $(window).on('scroll', function(event) {
+    $('body').removeClass('adv-condense-header');
+    $('#adv-edition-chooser').removeClass('adv-open');
+  });
+
+  // remove mouseovers that cover enter page, making them inescapable
+  // unable to get this working reliably based on enter/exit events, so just do
+  // it all the time.
+  $(window).mousemove(function() {
+    $('.adv-open').removeClass('adv-open');
+  });
+
+}).call(this)
